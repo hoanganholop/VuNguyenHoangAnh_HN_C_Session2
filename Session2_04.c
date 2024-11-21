bfec290c7b5b3c5a24d3468e1f4585f44fdc47c5
hoanganholop <hoanganholop@gmail.com>
2024-11-21 17:23:08 +0700

    Update Session2_04.c

--- Session2_04.c
+++ Session2_04.c
@@ -7,7 +7,6 @@ int main() {
     float perimeter = 4 * side;    
     float area = side * side;       
 
-    // In k?t qu? ra màn hình
     printf("Chu vi hinh vuong la: %.2f\n", perimeter); 
     printf("Dien tich hinh vuong la: %.2f\n", area);  
 
